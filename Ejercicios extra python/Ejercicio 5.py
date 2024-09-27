5ff67b1406b11b38e375ca81e584fa9ba03a2719
AdriàRey-ITB2425 <adria.rey.7e8@itb.cat>
2024-09-27 09:40:51 +0200

    Ejercicios Extra Basicos de Python

---
+++ Ejercicios extra python/Ejercicio 5.py
@@ -0,0 +1,13 @@
+Juan= 3
+Maria= 5
+Adan= 6
+total_manzanas= Juan+Maria+Adan
+print ("Manzanas de Juan",Juan, "Manzanas de Maria",Maria, "Manzanas de Adan",Adan)
+print()
+print ("Este es el total de las manzanas", total_manzanas)
+print()
+print ("Este es el total de las manzas de Juan multiplicadas por 8:", Juan*8)
+print()
+print ("Este resultado es el total de las multiplicaciones de las manzanas de todos:", Juan*Maria*Adan)
+print()
+print ("Y por ultimo este es el resultado de las multiplicacion entre las manzanas de Maria y Adan divididas por las de Juan:", Maria*Adan/Juan)
---
+++ Ejercicios extra python/Ejercicio 6.py
@@ -0,0 +1,7 @@
+a= float(input("Entra el primer valor:"))
+b= float(input("Entra el segundo valor:"))
+
+print("Suma:", a+b)
+print("Resta:", a-b)
+print("Multiplicacion:", a*b)
+print("Division:", a/b)
